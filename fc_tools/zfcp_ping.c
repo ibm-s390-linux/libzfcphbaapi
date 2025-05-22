2ef0c19b9580e7a94b3cc32e3342cc233aa340ae
Dan Horák <dan@danny.cz>
2025-05-22 09:21:30 +0000

    tools: use %lx format for WWPN
    Use %lx format when printing WWPN data stored in uint64_t variables/members
    as we are building on 64-bit systems exclusively and uint64_t is "long
    unsigned int" there.
    
    Signed-off-by: Dan Horák <dan@danny.cz>

--- fc_tools/zfcp_ping.c
+++ fc_tools/zfcp_ping.c
@@ -133,7 +133,7 @@ HBA_STATUS send_fc_ping(struct adapter_attr *aa, char *dest, unsigned int token,
         }
 
 	if (display_detail & VERBOSE)
-		printf("Sending PNG from BUS_ID=0.0.%x WWPN=0x%016llx "
+		printf("Sending PNG from BUS_ID=0.0.%x WWPN=0x%016lx "
 		       "ID=0x%x dev=%s speed=%s\n", aa->bus_id, aa->wwpn, 
 		       aa->d_id, aa->dev_name, speed[aa->speed]);
 	else
